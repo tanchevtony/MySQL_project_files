270d45c22dc2018923f40ab42b8b2e9c36d8deca
Tony <tony.tanchev@gmail.com>
2022-01-25 17:08:24 +0000

    Update create-databases.sql

--- create-databases.sql
+++ create-databases.sql
@@ -13,7 +13,7 @@ CREATE TABLE `payment_methods` (
 INSERT INTO `payment_methods` VALUES (1,'Credit Card');
 INSERT INTO `payment_methods` VALUES (2,'Cash');
 INSERT INTO `payment_methods` VALUES (3,'PayPal');
-INSERT INTO `payment_methods` VALUES (4,'Wire Transfer');
+INSERT INTO `payment_methods` VALUES (4,'Crypto');
 
 CREATE TABLE `clients` (
   `client_id` int(11) NOT NULL,
@@ -26,9 +26,10 @@ CREATE TABLE `clients` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 INSERT INTO `clients` VALUES (1,'Vinte','3 Nevada Parkway','Syracuse','NY','315-252-7305');
 INSERT INTO `clients` VALUES (2,'Myworks','34267 Glendale Parkway','Huntington','WV','304-659-1170');
-INSERT INTO `clients` VALUES (3,'Yadel','096 Pawling Parkway','San Francisco','CA','415-144-6037');
-INSERT INTO `clients` VALUES (4,'Kwideo','81674 Westerfield Circle','Waco','TX','254-750-0784');
-INSERT INTO `clients` VALUES (5,'Topiclounge','0863 Farmco Road','Portland','OR','971-888-9129');
+INSERT INTO `clients` VALUES (3,'Yadel','096 Bitterne Road','San Francisco','CA','415-144-6037');
+INSERT INTO `clients` VALUES (4,'Kwideo','81674 Park Gate Circle','Waco','TX','254-750-0784');
+INSERT INTO `clients` VALUES (5,'Topiclounge','0863 London Road','Portland','OR','971-888-9129');
+INSERT INTO `clients` VALUES (6,'Cilenia','100 Newton Road','Portland','OR','971-888-9129');
 
 CREATE TABLE `invoices` (
   `invoice_id` int(11) NOT NULL,
